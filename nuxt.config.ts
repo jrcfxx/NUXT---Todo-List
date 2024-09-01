b759d6000fa3c63d8f0daa4a3cce48ce46a5f527
jrcfxx <juliarochafiorini@gmail.com>
2024-09-01 20:04:51 -0300

    Modifying the axios plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,5 +25,7 @@ export default defineNuxtConfig({
       },
     },
   },
+
+  plugins: ['~/plugins/axios'],
 })
 
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,16 +1,28 @@
 // This plugin creates an instance of Axios and configures it with the baseURL of your Laravel API. 
 // Also adds the authentication token to each request, if available.
 
-export default function({ $axios }) {
-    // Intercepting every Axios request before it is sent
-    $axios.onRequest(config => {
-      // Retrieve the authentication token from the local storage
-      const token = localStorage.getItem('authToken'); 
-      // If the token exists, attach it to the Authorization header of the request
-      if (token) {
-        config.headers.common['Authorization'] = `Bearer ${token}`;
-      }
-      // Return the updated configuration object with the headers
-      return config;
-    });
-  }
+import axios from 'axios'
+
+export default defineNuxtPlugin(nuxtApp => {
+  // Creates an instance of Axios with the baseURL of the Laravel API
+  const api = axios.create({
+    baseURL: 'http://127.0.0.1:8000/api',
+  })
+
+  // Intercepting every Axios request before it is sent
+  api.interceptors.request.use(config => {
+// Retrieve the authentication token from the local storage
+    const token = localStorage.getItem('authToken')
+
+    // If the token exists, attach it to the Authorization header of the request
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    // Return the updated configuration object with the headers
+    return config
+  })
+
+  // Provides the configured Axios instance across the Nuxt application
+  nuxtApp.provide('api', api)
+})
