70f69eb7df5decbaf92f31e355783eca6b4657a5
jrcfxx <juliarochafiorini@gmail.com>
2024-10-17 08:55:29 -0300

    Removing unused code

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -3,27 +3,25 @@ import {useAuthStore} from '~/store/authStore'
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const store = useAuthStore();
 
-  
-    if (process.client) {
-        const token = store.loadFromLocalStorage(); 
+    const token = store.loadFromLocalStorage(); 
 
-        if (token) {
-            try {
-                const response = await store.getUserInfo();
-                
-                if (!response || response.error) {
-                    store.cleanStore();
-                    return navigateTo('/'); 
-                }
-            } catch (error) {
-                alert('Session expired');
+    if (token) {
+        try {
+            const response = await store.getUserInfo();
+            
+            if (!response || response.error) {
                 store.cleanStore();
-                return navigateTo('/');
+                return navigateTo('/'); 
             }
-        } else {
-            if (to.path !== '/') {
-                return navigateTo('/');
-            }
+        } catch (error) {
+            alert('Session expired');
+            store.cleanStore();
+            return navigateTo('/');
+        }
+    } else {
+        if (to.path !== '/') {
+            return navigateTo('/');
         }
     }
-});
+}
+);
