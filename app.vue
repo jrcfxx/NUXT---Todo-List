e47f205ac9cdf283c484a4e0e7dda9aa171057df
jrcfxx <juliarochafiorini@gmail.com>
2024-10-17 10:33:36 -0300

    removing unused code

--- app.vue
+++ app.vue
@@ -5,11 +5,3 @@
     </v-main>
   </v-app>
 </template>
-
-<script setup>
-import { useAuthStore } from '~/store/authStore';
-import { onMounted } from 'vue';
-
-const authStore = useAuthStore();
-
-</script>
